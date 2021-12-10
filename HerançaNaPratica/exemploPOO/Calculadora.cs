505b29b2a269d34f0ab040e65f97e1b790bff1d1
moisesgithub <moises.github@outlook.com>
2021-12-09 22:06:25 -0300

    Polimorfismo Tempo de Compilação Sobrecarga

---
+++ HerançaNaPratica/exemploPOO/Calculadora.cs
@@ -0,0 +1,15 @@
+namespace exemploPOO
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public int Somar(int a, int b, int c)
+        {
+            return a + b + c;
+        }    
+    }
+}
--- HerançaNaPratica/exemploPOO/Program.cs
+++ HerançaNaPratica/exemploPOO/Program.cs
@@ -6,7 +6,12 @@ namespace exemploPOO
     {
         static void Main(string[] args)
         {
-            Aluno al = new Aluno();
+            Calculadora calc = new Calculadora();//instancia da classe Calculadora
+            Console.WriteLine("Resultado da 1ª soma: " + calc.Somar(10, 20));//chamando o método somar da classe Calculadora
+            Console.WriteLine("Resultado da 2ª soma: " + calc.Somar(10, 20, 30));
+            
+            
+            /*Aluno al = new Aluno();
             al.Nome = "João";
             al.Idade = 20;
             al.Matricula = "12345";
@@ -20,7 +25,7 @@ namespace exemploPOO
             prof.Matricula = "12345";
             prof.Salario = 6000;
 
-            prof.Apresentar();
+            prof.Apresentar();*/
         }
     }
 }
